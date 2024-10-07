c8882c0625dcccf8ea43426cfa06b6d311a19341
Ronak Chauhan <ronak@cs.wisc.edu>
2024-10-07 15:40:59 -0500

    [AMDGPU] Add CMake option for AMDGPU build of Dyninst

--- cmake/DyninstOptions.cmake
+++ cmake/DyninstOptions.cmake
@@ -34,6 +34,8 @@ option(DYNINST_WARNINGS_AS_ERRORS "Treat compilation warnings as errors" OFF)
 
 option(ENABLE_PARSE_API_GRAPHS "Enable Boost Graph wrappers for parseAPI Functions" OFF)
 
+option(DYNINST_ARCH_amdgpu "Special AMDGPU specific build" OFF)
+
 set(DYNINST_LINKER
     ""
     CACHE STRING "The linker to use")
